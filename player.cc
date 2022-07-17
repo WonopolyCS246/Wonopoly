87d456361d6828707150a6f6158bf967385c692e
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2022-07-17 13:51:29 -0400

    Update player.cc

--- player.cc
+++ player.cc
@@ -1,1 +1,44 @@
 #include "player.h"
+
+Player::Player(string name, int assets, int position){
+  
+}
+
+std::vector<Property*> Player::getProp(){
+  
+}
+
+void Player::addProp(Property* p){
+  
+}
+int Player::addAssets(){
+  
+}
+// Get Assets was not present in UML
+int Player::getAssets(){
+  
+}
+void Player::setAssets(int assests){
+  
+}
+void Player::setBankruptcy(bool B){
+  
+}
+int Player::getRUTR(){
+  
+}
+void Player::setRUTR(){
+  
+}
+
+bool Player::getAtTims(){
+  
+}
+
+void Player::setAtTims(){
+  
+}
+
+void Player::transfer(Player* p){ 
+  
+}
