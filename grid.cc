579c0c8163d65a9c4f8b57c36405dd3d09d2ee76
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2022-07-17 12:44:15 -0400

    Update grid.cc

--- grid.cc
+++ grid.cc
@@ -1,2 +1,71 @@
 #include grid.h
 
+<Property *> Grid::card{
+
+}
+
+<Players *> players{
+
+}
+<string, int> piece{
+
+}
+
+void roll(){
+
+}
+
+void nextTurn(){
+
+}
+
+void trade(){
+
+}
+
+Property* getProperty(string propertyName){
+
+}
+
+void Grid:: buyProperty(Property* p){
+
+}
+
+void Grid::sellProperty(Property* p){
+
+}
+
+void Grid::mortgage(Property* p){
+
+}
+
+void Grid::unmortgage(Property* p){
+
+}
+
+
+bool Grid::bankrupt(){
+
+}
+
+int Grid::assets(){
+
+}
+
+int Grid::all{
+
+}
+// Explicitly defining the type of auction
+void Grid::auctionProperty(Property * p){
+
+}
+// auctionPlayer() calls auctionProperty() on all properties owned by player 
+void Grid::auctionPlayer(Player * p){
+
+}
+
+void Grid::transfer(Player * p1, Player * p2){
+
+}
+
+    
