6ff5dd98fe3a271e128219c00c3a1151e2ccd579
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2022-07-17 13:40:23 -0400

    roll2die made

---
+++ roll2die.cc
@@ -0,0 +1,19 @@
+#include "roll2die.h"
+
+std::vector<int> roll2die(){
+    // Defining two vectors 
+    // Dice rep the faces of die
+    std::vector<int> Dice = { 1, 2, 3, 4, 5, 6 };
+    // result vector which will be returned
+    std::vector<int> result;
+    // Using the epoch time as the seed for random engine
+	unsigned seed = 
+std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine rng{seed};
+
+	std::shuffle( Dice.begin(), Dice.end(), rng );
+	result[0] = Dice[0];
+    result[1] = Dice[1];
+    return result;
+}
+
---
+++ roll2die.h
@@ -0,0 +1,16 @@
+#ifndef ROLL_2_DIE
+#define ROLL_2_DIE
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+
+// This will result a list of 2 ints (between 1 and 6, both inclusive)
+std::vector<int> roll2die();
+
+
+#endif
