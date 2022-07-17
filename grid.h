35c7cdf575018002f58becccd94daafc9a31d453
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2022-07-17 12:38:37 -0400

    Update grid.h

--- grid.h
+++ grid.h
@@ -8,22 +8,23 @@ class Grid {
   <Property *> card;
   <Players *> players;
   <string, int> piece;
-  void roll();
-  void nextTurn();
-  void trade();
-  Property* getProperty(string propertyName);
-  void buyProperty(Property* p);
-  void sellProperty(Property* p);
-  void mortgage(Property* p);
-  void unmortgage(Property* p);
-  bool bankrupt();
-  int assets();
-  int all;
-  // Explicitly defining the type of auction
-  void auctionProperty(Property * p);
-  // auctionPlayer() calls auctionProperty() on all properties owned by player 
-  void auctionPlayer(Player * p);
-  void transfer(Player * p1, Player * p2);
+  public:
+    void roll();
+    void nextTurn();
+    void trade();
+    Property* getProperty(string propertyName);
+    void buyProperty(Property* p);
+    void sellProperty(Property* p);
+    void mortgage(Property* p);
+    void unmortgage(Property* p);
+    bool bankrupt();
+    int assets();
+    int all;
+    // Explicitly defining the type of auction
+    void auctionProperty(Property * p);
+    // auctionPlayer() calls auctionProperty() on all properties owned by player 
+    void auctionPlayer(Player * p);
+    void transfer(Player * p1, Player * p2);
   
 };
 
