c9d32479b0a2c9f2697818dc5f92b01ab10188e5
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2022-07-17 12:22:00 -0400

    Player.h made

--- player.h
+++ player.h
@@ -6,6 +6,34 @@ class Property;
 
 class Player {
     string name;
+    <Property*> owned;
+    int assets;
+    bool bankruptcy;
+    // TutionPaid was not present in the UML
+    bool tutionPaid;
+    int position;
+    int RUTR;
+    bool atTims;
+    public:
+        Player(string name. int assets, int position);
+        <Property*> getProp();
+        void addProp(Property* p);
+        int addAssets();
+        // Get Assets was not present in UML
+        int getAssets();
+        void setAssets(int assests);
+        void setBankruptcy(bool B);
+        int getRUTR();
+        void setRUTR();
+        bool getAtTims();
+        void setAtTims();
+        void transfer(Player* p)
+};
+
+
+/*
+class Player {
+    string name;
     std::vector<Property*> owned;
     bool tutionPaid;
     bool bankrupt;
@@ -25,3 +53,4 @@ class Player {
         int getTimCount();
         void setTimCount(int count);
 };
+*/
