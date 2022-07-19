22cb33a728a6b00af5eb434af2836eecd9c60041
Medhansh-Hinduja <medhs2011@gmail.com>
2022-07-19 19:01:33 -0400

    Hinduja first commit

--- player.h
+++ player.h
@@ -14,14 +14,14 @@ class Player
     bool tutionPaid;
     int position;
     int previousPosition; // this has been added keeping in mind that previousPosition must be know for graphic purposes
-    int RUTR;             // number of rim up the tims card used.
+    int RUTR;             // number of roll up the rim cards used.
     bool atTims;
 
 public:
     Player(std::string name, int assets, int position);
     std::vector<Property *> getProp();
     void addProp(Property *p);
-    void addAssets(int amount); // note intially int amount was not given but to my best understanding it should accept a parameter of type int.
+    void addAssets(int amount); // note initially int amount was not given but to my best understanding it should accept a parameter of type int.
     // Get Assets was not present in UML
     int getAssets();
     void setAssets(int assests);
