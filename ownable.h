167cd8f0d15f6b146a6f5ac37a551297a988f5b8
VinayakBector2002 <bector.vinayak02@gmail.com>
2022-07-23 18:24:54 -0400

    ownable .h

--- ownable.h
+++ ownable.h
@@ -12,23 +12,34 @@
 class Player; // forward declaration
 class Ownable : public Property, public Subject, public Observer
 {
-    State state;
+
     Info info;
-    std::vector<Subject *> neighbours; // stores the neighbours of the property
+    // stores the neighbours of the property
 public:
     Ownable(Player *owner, std::string name, int position, Faculty faculty, int increments, bool mortaged);
     void notify(Subject &whoFrom) override;
-    void setState(State newS);
-    void attach(Observer *o) override; // this is to attach the observer
+    std::string getName() override;
+    void attach(Property *o) override;
+
+    void addincrement(Player *p); // this is to increment the particular monopolies of the faculty
+
     Info getInfo() override;
     void addOwner(Player *p) override;
     bool isNewOwnable() override;
     void applyRule(Player *p) override;
     Player *getOwner() override;
+
+    bool isMortgaged() override;
+
+    int getPrice() override;
     void setMortgaged(Player *p) override; // this is to set the mortgaged status of the property
+
     void unMortgaged(Player *p) override; // // this is to unmortage the property
+
     void removeincrement(Player *p) override; // this removes just one increment of the monopoly
     void addincrement(Player *p) override;    // this is to add increments to property.
+
+    void setMortgaged(bool mortgaged) override;
 };
 
 #endif
