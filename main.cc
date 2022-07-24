674c8c72f186fe73af0a5feb7ea2db8413c057e2
VinayakBector2002 <bector.vinayak02@gmail.com>
2022-07-24 02:28:25 -0400

    commit

--- main.cc
+++ main.cc
@@ -8,38 +8,32 @@
 using namespace std;
 
 int main(int argc, char **argv){
-    if (argc == 1) { //./monopoly
-        Grid g;
-        g.play();
+    bool chk = false;
+    if (argc == 1) {
+        
     } else if (argc == 2) { // ./monopoly -testing
         istringstream ss{argv[1]};
         string s;
         ss >> s;
         if (s == "-testing") {
+            chk = true;
             // Do whatever the testing flag means
-            //Grid g;
-            //g.play();
-            cout << " testing " << endl;
-        } else {
-            cout << "Format: ./monopoly -load file -testing" << endl;
-            cout << "You can omit either -load file and/or -testing" << endl;
-            return 1;
+            cout << "Welcome to Watopoly!" << endl;
+            Grid g;
+            g.play();
         }
     } else if (argc == 3) { // ./monopoly -load file
         istringstream ss{argv[1]};
         string s;
         ss >> s;
         if (s == "-load") {
+            chk = true;
             istringstream ss1{argv[2]};
             string s1;
             ss1 >> s1;
-            cout << " ./monopoly -load file "<< argv[2] << endl;
-            //Grid g{ifstream{s1}};
-            //g.play();
-        }  else {
-            cout << "Format: ./monopoly -load file -testing" << endl;
-            cout << "You can omit either -load file and/or -testing" << endl;
-            return 1;
+            cout << "Welcome to Watopoly!" << endl;
+            Grid g{ifstream{s1}};
+            g.play();
         }
     } else if (argc == 4) { // ./monopoly -load file -testing
         istringstream ss{argv[1]};
@@ -52,16 +46,19 @@ int main(int argc, char **argv){
         string s2;
         ss2 >> s2;
         if (s == "-load" && s2 == "-testing") {
-            //Grid g{ifstream{s1}};
-            //g.play();
-            cout << "-loading and -testing" << endl;
+            cout << "Welcome to Watopoly!" << endl;
+            chk = true;
+            Grid g{ifstream{s1}};
+            g.play();
             // do whatever the -testing flag means
-        } else {
-            cout << "Format: ./monopoly -load file -testing" << endl;
-            cout << "You can omit either -load file and/or -testing" << endl;
-            return 1;
+        } else if (s == "-testing" && s1 == "-load") {
+            cout << "Welcome to Watopoly!" << endl;
+            chk = true;
+            Grid g{ifstream{s2}};
+            g.play();
         }
-    } else {
+    } 
+    if (!chk) {
         cout << "Format: ./monopoly -load file -testing" << endl;
         cout << "You can omit either -load file and/or -testing" << endl;
         return 1;
